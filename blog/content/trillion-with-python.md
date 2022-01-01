9078ebeac67902b7aabc4a45141cae2fb6baa9fb
matejcik <ja@matejcik.cz>
2022-01-01 11:59:06 +0100

    pelican blog draft

---
+++ blog/content/trillion-with-python.md
@@ -0,0 +1,15 @@
+Title: 22 trillion parameters in Python
+Date: 2022-01-01 11:00
+Category: Python
+Slug: trillion-py
+Summary: bla bla for now
+
+Hello, world.
+
+```python
+class ALU:
+    def __init__(self) -> None:
+        self.regs = {"x": 0, "y": 0, "z": 0, "w": 0}
+        self.min = 0
+        self.max = 0
+```
---
+++ blog/pelicanconf.py
@@ -0,0 +1,26 @@
+AUTHOR = 'matejcik'
+SITENAME = 'advent things'
+SITEURL = 'https://advent.matejcik.cz'
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Prague'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+# LINKS = (('Pelican', 'https://getpelican.com/'),
+#          ('Python.org', 'https://www.python.org/'),
+#          ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+#          ('Simplegrey', 'https://github.com/fle/pelican-simplegrey/tree/257e30c7e0091df2198b2c778754cd6f23112068'),)
+
+# Social widget
+SOCIAL = (('GitHub', 'https://github.com/matejcik/advent'),
+          ('Reddit', 'https://www.reddit.com/user/matejcik/'),)
