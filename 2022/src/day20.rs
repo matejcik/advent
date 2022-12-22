f96b236bfa8a5144ab3885b51d1b9ee40b87602b
matejcik <ja@matejcik.cz>
2022-12-22 18:03:58 +0100

    day 20: chunk lookup optimization, 500us / 4.5ms runtime

--- 2022/src/day20.rs
+++ 2022/src/day20.rs
@@ -4,9 +4,18 @@ use bstr::io::BufReadExt;
 
 use crate::{parse_num, Solver};
 
-struct SparseVec<T>(Vec<Vec<T>>);
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct Entry {
+    index: u16,
+    value: i16,
+}
 
-impl<T: Clone> SparseVec<T> {
+struct Mixer {
+    chunks: Vec<Vec<Entry>>,
+    indices: Vec<u16>,
+}
+
+impl Mixer {
     const CHUNK_SIZE: usize = 128;
     const CHUNK_CAPACITY: usize = 128 + 64;
 
@@ -16,14 +25,38 @@ impl<T: Clone> SparseVec<T> {
         for _ in 0..chunks {
             chunk_vec.push(Vec::with_capacity(Self::CHUNK_CAPACITY));
         }
-        Self(chunk_vec)
+        Self {
+            chunks: chunk_vec,
+            indices: Vec::with_capacity(capacity),
+        }
+    }
+
+    fn chunk_and_index(&self, global_index: usize) -> (usize, usize) {
+        let mut total = 0;
+        for (chunk_idx, chunk) in self.chunks.iter().enumerate() {
+            if global_index < total + chunk.len() {
+                return (chunk_idx, global_index - total);
+            }
+            total += chunk.len();
+        }
+        panic!("index out of bounds");
+    }
+
+    fn global_index(&self, chunk: usize, index: usize) -> usize {
+        let chunk_offset = self
+            .chunks
+            .iter()
+            .take(chunk)
+            .map(|chunk| chunk.len())
+            .sum::<usize>();
+        chunk_offset + index
     }
 
     fn rebalance(&mut self) {
         let len = self.len();
-        let mut new_vec = Vec::with_capacity(self.0.len() * Self::CHUNK_SIZE);
+        let mut new_vec = Vec::with_capacity(self.chunks.len() * Self::CHUNK_SIZE);
         let mut chunk = Vec::with_capacity(Self::CHUNK_CAPACITY);
-        for prev_chunk in self.0.iter() {
+        for prev_chunk in self.chunks.iter() {
             let mut prev_slice = prev_chunk.as_slice();
             while prev_slice.len() >= Self::CHUNK_SIZE - chunk.len() {
                 let remain = Self::CHUNK_SIZE - chunk.len();
@@ -37,94 +70,78 @@ impl<T: Clone> SparseVec<T> {
         if chunk.len() > 0 {
             new_vec.push(chunk);
         }
-        self.0 = new_vec;
+        for c in 0..new_vec.len() {
+            for i in 0..new_vec[c].len() {
+                let entry = new_vec[c][i];
+                self.indices[entry.index as usize] = c as u16;
+            }
+        }
+        self.chunks = new_vec;
         assert_eq!(len, self.len());
     }
 
-    pub fn insert(&mut self, idx: usize, item: T) {
-        let mut total = 0;
-        for chunk in self.0.iter_mut() {
-            if idx <= total + chunk.len() {
-                chunk.insert(idx - total, item);
-                if chunk.len() >= chunk.capacity() {
-                    self.rebalance();
-                }
-                return;
-            }
-            total += chunk.len();
+    pub fn mix_item(&mut self, item: usize, encryption_key: i64) {
+        let src_chunk_idx = self.indices[item] as usize;
+        let chunk = &self.chunks[src_chunk_idx];
+        let src_index = chunk
+            .iter()
+            .position(|entry| entry.index == item as u16)
+            .unwrap();
+        let src_global_index = self.global_index(src_chunk_idx, src_index);
+
+        let entry = chunk[src_index];
+
+        let value = entry.value as i64 * encryption_key;
+        // calculate shift
+        let dest_global_index =
+            (src_global_index as i64 + value).rem_euclid(self.len() as i64 - 1) as usize;
+        if dest_global_index == src_global_index {
+            // shift is zero, do nothing
+            return;
         }
-        panic!("index out of bounds");
+        // drop item from source chunk
+        self.chunks[src_chunk_idx].remove(src_index);
+        // calculate destination chunk and index
+        // (doing this after item is dropped because it affects the global index)
+        let (mut dest_chunk, mut dest_i) = self.chunk_and_index(dest_global_index);
+        // update the chunk index in indices
+        if dest_chunk > 0 && dest_i == 0 {
+            // prefer pushing back into a previous chunk instead of inserting front of next chunk
+            dest_chunk -= 1;
+            dest_i = self.chunks[dest_chunk].len();
+        }
+        self.indices[item] = dest_chunk as u16;
+        // insert item into destination chunk
+        self.chunks[dest_chunk].insert(dest_i, entry);
+        if self.chunks[dest_chunk].len() >= self.chunks[dest_chunk].capacity()
+            || self.chunks[src_chunk_idx].len() == 0
+        {
+            // rebalance if the destination chunk is full or the source chunk is empty
+            self.rebalance();
+        }
     }
 
-    pub fn remove(&mut self, idx: usize) {
-        let mut total = 0;
-        for chunk in self.0.iter_mut() {
-            if idx < total + chunk.len() {
-                chunk.remove(idx - total);
-                if chunk.is_empty() {
-                    self.rebalance();
-                }
-                return;
-            }
-            total += chunk.len();
+    pub fn mix(&mut self, encryption_key: i64) {
+        for i in 0..self.len() {
+            self.mix_item(i, encryption_key);
         }
-        panic!("index out of bounds");
     }
 
-    pub fn iter_from(&self, idx: usize) -> impl Iterator<Item = &T> + Clone + '_ {
-        let mut total = 0;
-        for (chunk_idx, chunk) in self.0.iter().enumerate() {
-            if idx < total + chunk.len() {
-                return SparseVecIter {
-                    sparse_vec: self,
-                    chunk_idx,
-                    item_idx: idx - total,
-                };
-            }
-            total += chunk.len();
-        }
-        panic!("index out of bounds");
+    pub fn iter(&self) -> impl Iterator<Item = &Entry> + Clone {
+        self.chunks.iter().flatten()
     }
 
     pub fn len(&self) -> usize {
-        self.0.iter().map(|chunk| chunk.len()).sum()
+        self.chunks.iter().map(|chunk| chunk.len()).sum()
     }
 }
 
-#[derive(Clone)]
-struct SparseVecIter<'a, T: Clone> {
-    sparse_vec: &'a SparseVec<T>,
-    chunk_idx: usize,
-    item_idx: usize,
-}
-
-impl<'a, T: Clone> Iterator for SparseVecIter<'a, T> {
-    type Item = &'a T;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.chunk_idx >= self.sparse_vec.0.len() {
-            self.chunk_idx = 0;
-            self.item_idx = 0;
-            return self.next();
-        }
-        let chunk = &self.sparse_vec.0[self.chunk_idx];
-        if self.item_idx >= chunk.len() {
-            self.chunk_idx += 1;
-            self.item_idx = 0;
-            return self.next();
-        }
-        let item = &chunk[self.item_idx];
-        self.item_idx += 1;
-        Some(item)
-    }
-}
-
-impl<T> Index<usize> for SparseVec<T> {
-    type Output = T;
+impl Index<usize> for Mixer {
+    type Output = Entry;
 
     fn index(&self, idx: usize) -> &Self::Output {
         let mut total = 0;
-        for chunk in self.0.iter() {
+        for chunk in self.chunks.iter() {
             if idx < total + chunk.len() {
                 return &chunk[idx - total];
             }
@@ -134,14 +151,20 @@ impl<T> Index<usize> for SparseVec<T> {
     }
 }
 
-impl<T: Clone> From<Vec<T>> for SparseVec<T> {
-    fn from(vec: Vec<T>) -> Self {
+impl From<Vec<i16>> for Mixer {
+    fn from(vec: Vec<i16>) -> Self {
         let mut sparse_vec = Self::with_capacity(vec.len());
-        for (i, chunk) in sparse_vec.0.iter_mut().enumerate() {
+        for (i, chunk) in sparse_vec.chunks.iter_mut().enumerate() {
             let start = i * Self::CHUNK_SIZE;
             let end = vec.len().min((i + 1) * Self::CHUNK_SIZE);
-            chunk.extend_from_slice(&vec[start..end]);
+            chunk.extend(vec[start..end].iter().enumerate().map(|(ii, n)| Entry {
+                index: (start + ii) as u16,
+                value: *n,
+            }));
         }
+        sparse_vec.indices = (0..vec.len())
+            .map(|n| (n / Self::CHUNK_SIZE) as u16)
+            .collect();
         sparse_vec
     }
 }
@@ -154,43 +177,20 @@ fn parse_num_negative(line: &[u8]) -> i64 {
     }
 }
 
-fn do_mix(data: &mut SparseVec<(usize, i64)>, start_finger: usize) -> usize {
-    let len = data.len();
-    let mut finger = start_finger;
-    let mut cur = 0;
-    while cur < len {
-        finger = (finger
-            + data
-                .iter_from(finger)
-                .position(|&(idx, _)| idx == cur)
-                .unwrap())
-            % len;
-        let (idx, n) = data[finger];
-        assert_eq!(idx, cur);
-        let dest_index = (finger as i64 + n).rem_euclid(len as i64 - 1) as usize;
-        data.remove(finger);
-        data.insert(dest_index, (idx, n));
-        cur += 1;
-    }
-    finger
-}
-
 fn part1_mix_once(input: &mut dyn BufRead) -> String {
     let data = input
         .byte_lines()
         .flatten()
-        .map(|line| parse_num_negative(&line) as i64)
-        .enumerate()
+        .map(|line| parse_num_negative(&line) as i16)
         .collect::<Vec<_>>();
 
-    let mut sparsevec = SparseVec::from(data);
+    let mut sparsevec = Mixer::from(data);
+    sparsevec.mix(1);
 
-    do_mix(&mut sparsevec, 0);
-
-    let zero_idx = sparsevec.iter_from(0).position(|(_, n)| *n == 0).unwrap();
+    let zero_idx = sparsevec.iter().position(|e| e.value == 0).unwrap();
     sparsevec
-        .iter_from(0)
-        .map(|(_, n)| *n)
+        .iter()
+        .map(|e| e.value as i64)
         .cycle()
         .skip(zero_idx)
         .step_by(1000)
@@ -205,21 +205,17 @@ fn part2_mix_ten_times(input: &mut dyn BufRead) -> String {
     let data = input
         .byte_lines()
         .flatten()
-        .map(|line| parse_num_negative(&line) as i64 * DECRYPTION_KEY)
-        .enumerate()
+        .map(|line| parse_num_negative(&line) as i16)
         .collect::<Vec<_>>();
 
-    let mut sparsevec = SparseVec::from(data);
-
-    let mut finger = 0;
+    let mut sparsevec = Mixer::from(data);
     for _ in 0..10 {
-        finger = do_mix(&mut sparsevec, finger);
+        sparsevec.mix(DECRYPTION_KEY);
     }
-
-    let zero_idx = sparsevec.iter_from(0).position(|(_, n)| *n == 0).unwrap();
+    let zero_idx = sparsevec.iter().position(|e| e.value == 0).unwrap();
     sparsevec
-        .iter_from(0)
-        .map(|(_, n)| *n)
+        .iter()
+        .map(|e| e.value as i64 * DECRYPTION_KEY)
         .cycle()
         .skip(zero_idx)
         .step_by(1000)
