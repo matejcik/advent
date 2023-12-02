9715f3c5e362bd127fda14c20ed16cd4e5227265
matejcik <ja@matejcik.cz>
2023-12-02 20:08:46 +0100

    2023: harness + day 2 skeleton

---
+++ 2023/src/02.zig
@@ -0,0 +1,9 @@
+const std = @import("std");
+
+pub fn part1(reader: std.io.StreamSource.Reader) !void {
+    _ = reader;
+}
+
+pub fn part2(reader: std.io.StreamSource.Reader) !void {
+    _ = reader;
+}
--- 2023/src/main.zig
+++ 2023/src/main.zig
@@ -2,12 +2,25 @@ const std = @import("std");
 
 const part1 = @import("01.zig");
 
-pub fn main() !void {
-    const file = try std.fs.cwd().openFile("inputs/01.txt", .{ .mode = .read_only });
-    var source = std.io.StreamSource{ .file = file };
-    try part1.part1(source.reader());
+const PARTS = [_]type{
+    @import("01.zig"),
+    @import("02.zig"),
+};
+
+fn runSingle(comptime n: u32, comptime solver_impl: type) !void {
+    const file_stem = std.fmt.comptimePrint("{:02}", .{n});
+    const input_file = try std.fs.cwd().openFile("inputs/" ++ file_stem ++ ".txt", .{ .mode = .read_only });
+    var source = std.io.StreamSource{ .file = input_file };
+
+    try solver_impl.part1(source.reader());
     try source.seekTo(0);
-    try part1.part2(source.reader());
+    try solver_impl.part2(source.reader());
+}
+
+pub fn main() !void {
+    inline for (PARTS, 0..) |part, n| {
+        try runSingle(n + 1, part);
+    }
 }
 
 test "simple test" {
