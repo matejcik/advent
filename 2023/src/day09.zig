b912f559cbda5105d8100e169d9e59c7222fa424
matejcik <ja@matejcik.cz>
2023-12-09 21:53:59 +0100

    2023: day 9

---
+++ 2023/src/day09.zig
@@ -0,0 +1,84 @@
+const std = @import("std");
+const print = std.debug.print;
+const expect = std.debug.expect;
+
+fn nextElem(data: []isize) isize {
+    if (data.len == 0) {
+        return 0;
+    }
+    if (data.len == 1) {
+        return data[0];
+    }
+
+    var all_same_diff = false;
+    var end = data.len;
+    while (!all_same_diff) {
+        const first_diff = data[1] - data[0];
+        all_same_diff = true;
+        for (0..end - 1) |i| {
+            data[i] = data[i + 1] - data[i];
+            if (data[i] != first_diff) {
+                all_same_diff = false;
+            }
+        }
+        end -= 1;
+        if (end == 0) {
+            unreachable;
+        }
+    }
+    // extrapolate
+    var extrap: isize = data[end - 1];
+    for (end..data.len) |i| {
+        extrap += data[i];
+    }
+    return extrap;
+}
+
+test "extrapolation examples" {
+    var ex1 = [_]isize{ 0, 3, 6, 9, 12, 15 };
+    try std.testing.expectEqual(nextElem(&ex1), 18);
+
+    var ex2 = [_]isize{ 1, 3, 6, 10, 15, 21 };
+    try std.testing.expectEqual(nextElem(&ex2), 28);
+
+    var ex3 = [_]isize{ 10, 13, 16, 21, 30, 45 };
+    try std.testing.expectEqual(nextElem(&ex3), 68);
+}
+
+pub fn part1(data: []const u8, alloc: std.mem.Allocator) !void {
+    var linebuf = std.ArrayList(isize).init(alloc);
+    defer linebuf.deinit();
+
+    var total: isize = 0;
+    var lines_iter = std.mem.tokenizeScalar(u8, data, '\n');
+    while (lines_iter.next()) |line| {
+        linebuf.clearRetainingCapacity();
+        var line_iter = std.mem.tokenizeScalar(u8, line, ' ');
+        while (line_iter.next()) |num_str| {
+            const num = try std.fmt.parseInt(isize, num_str, 10);
+            try linebuf.append(num);
+        }
+        total += nextElem(linebuf.items);
+    }
+
+    print("Day 9 part 1: {}\n", .{total});
+}
+
+pub fn part2(data: []const u8, alloc: std.mem.Allocator) !void {
+    var linebuf = std.ArrayList(isize).init(alloc);
+    defer linebuf.deinit();
+
+    var total: isize = 0;
+    var lines_iter = std.mem.tokenizeScalar(u8, data, '\n');
+    while (lines_iter.next()) |line| {
+        linebuf.clearRetainingCapacity();
+        var line_iter = std.mem.tokenizeScalar(u8, line, ' ');
+        while (line_iter.next()) |num_str| {
+            const num = try std.fmt.parseInt(isize, num_str, 10);
+            try linebuf.insert(0, num);
+        }
+        total += nextElem(linebuf.items);
+    }
+
+    print("Day 9 part 2: {}\n", .{total});
+}
--- 2023/src/main.zig
+++ 2023/src/main.zig
@@ -11,6 +11,7 @@ const PARTS = [_]type{
     @import("day06"),
     @import("day07"),
     @import("day08"),
+    @import("day09"),
 };
 
 fn runSingle(comptime n: u32, comptime solver_impl: type) !void {
