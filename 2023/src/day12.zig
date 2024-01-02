2fceae8b7792337c2d7939c45580875538558067
matejcik <ja@matejcik.cz>
2024-01-02 14:55:03 +0100

    2023: day 12
    expanded to accomodate part 2
    and fixed the dynamic algorithm with all the "check if you didn't leave an empty space at one side" code

--- 2023/src/day12.zig
+++ 2023/src/day12.zig
@@ -2,20 +2,20 @@ const std = @import("std");
 const expect = std.testing.expect;
 const expectEqual = std.testing.expectEqual;
 
-const MAX_SEGMENTS = 10;
-const MAX_WIDTH = 64;
+const MAX_SEGMENTS = 30;
+const MAX_WIDTH = 128;
 
-const ONE: u64 = 1;
+const ONE: u128 = 1;
 
-fn sum(self: []const u32) u32 {
-    var res: u32 = 0;
+fn sum(comptime T: type, self: []const T) T {
+    var res: T = 0;
     for (self) |n| {
         res += n;
     }
     return res;
 }
 
-fn printBin(mask: u64, width: usize) void {
+fn printBin(mask: u128, width: usize) void {
     for (0..width) |i| {
         const ch: u8 = if (mask & (ONE << @intCast(i)) > 0) '#' else '.';
         std.debug.print("{c}", .{ch});
@@ -24,18 +24,18 @@ fn printBin(mask: u64, width: usize) void {
 }
 
 const Mask = struct {
-    num_mask: u64,
-    spaces_mask: u64,
-    width_mask: u64,
+    num_mask: u128,
+    spaces_mask: u128,
+    width_mask: u128,
 
     pub fn init(num: u32, width: usize) Mask {
-        var num_mask: u64 = 0;
+        var num_mask: u128 = 0;
         for (0..num) |i| {
             num_mask |= ONE << @intCast(i);
         }
-        const spaces_mask: u64 = ONE << @intCast(num + 1) | ONE;
+        const spaces_mask: u128 = ONE << @intCast(num + 1) | ONE;
 
-        var width_mask: u64 = 0;
+        var width_mask: u128 = 0;
         for (0..width) |i| {
             width_mask |= ONE << @intCast(i);
         }
@@ -47,12 +47,12 @@ const Mask = struct {
         };
     }
 
-    pub fn testNum(self: Mask, pattern: u64, pos: usize) bool {
+    pub fn testNum(self: Mask, pattern: u128, pos: usize) bool {
         const num_at = self.num_mask << @intCast(pos);
         return (pattern & num_at) == (num_at & self.width_mask);
     }
 
-    pub fn testNumDebug(self: Mask, pattern: u64, pos: usize, width: usize) bool {
+    pub fn testNumDebug(self: Mask, pattern: u128, pos: usize, width: usize) bool {
         std.debug.print("orig num:   ", .{});
         printBin(self.num_mask, width);
         std.debug.print("moved num:  ", .{});
@@ -67,7 +67,7 @@ const Mask = struct {
         return (pattern & num_at) == (num_at & self.width_mask);
     }
 
-    pub fn testSpaces(self: Mask, pattern: u64, pos: usize) bool {
+    pub fn testSpaces(self: Mask, pattern: u128, pos: usize) bool {
         const spaces_at = ((self.spaces_mask << @intCast(pos)) >> 1) & self.width_mask;
         return (pattern & spaces_at) == spaces_at;
     }
@@ -75,15 +75,16 @@ const Mask = struct {
 
 const Record = struct {
     width: usize,
-    pattern: []const u8,
-    can_be_used_mask: u64,
-    can_be_empty_mask: u64,
+    used_mask: u128,
+    empty_mask: u128,
+    any_mask: u128,
     segments_buf: [MAX_SEGMENTS]u32,
     segments_len: usize,
 
     pub fn parse(line: []const u8) Record {
-        var can_be_used_mask: u64 = 0;
-        var can_be_empty_mask: u64 = 0;
+        var used_mask: u128 = 0;
+        var empty_mask: u128 = 0;
+        var any_mask: u128 = 0;
         var segments_buf = [_]u32{0} ** MAX_SEGMENTS;
         // generate the masks and calculate width
         var width: usize = 0;
@@ -93,12 +94,9 @@ const Record = struct {
                     width = i;
                     break;
                 },
-                '#' => can_be_used_mask |= ONE << @intCast(i),
-                '.' => can_be_empty_mask |= ONE << @intCast(i),
-                '?' => {
-                    can_be_used_mask |= ONE << @intCast(i);
-                    can_be_empty_mask |= ONE << @intCast(i);
-                },
+                '#' => used_mask |= ONE << @intCast(i),
+                '.' => empty_mask |= ONE << @intCast(i),
+                '?' => any_mask |= ONE << @intCast(i),
                 else => unreachable,
             }
         }
@@ -113,25 +111,53 @@ const Record = struct {
             segments_len += 1;
         }
         return Record{
-            .pattern = line[0..width],
             .width = width,
-            .can_be_used_mask = can_be_used_mask,
-            .can_be_empty_mask = can_be_empty_mask,
+            .used_mask = used_mask,
+            .empty_mask = empty_mask,
+            .any_mask = any_mask,
             .segments_buf = segments_buf,
             .segments_len = segments_len,
         };
     }
 
-    pub fn dynCountOptions(self: Record) u32 {
+    pub fn expand(self: *Record, comptime factor: u8) void {
+        std.debug.assert(self.width * factor <= MAX_WIDTH);
+        std.debug.assert(self.segments_len * factor <= MAX_SEGMENTS);
+
+        var used_mask = self.used_mask;
+        var empty_mask = self.empty_mask;
+        var any_mask = self.any_mask;
+
+        inline for (1..factor) |i| {
+            const w = (self.width + 1) * i;
+            used_mask |= self.used_mask << @intCast(w);
+            empty_mask |= self.empty_mask << @intCast(w);
+            any_mask |= self.any_mask << @intCast(w);
+            any_mask |= ONE << @intCast(w - 1);
+
+            const s = self.segments_len * i;
+            for (0..self.segments_len) |n| {
+                self.segments_buf[s + n] = self.segments_buf[n];
+            }
+        }
+
+        self.used_mask = used_mask;
+        self.empty_mask = empty_mask;
+        self.any_mask = any_mask;
+        self.width = self.width * factor + factor - 1;
+        self.segments_len *= factor;
+    }
+
+    pub fn dynCountOptions(self: Record) u64 {
         // Segment dynamic buffers.
-        var a = [_]u32{0} ** MAX_WIDTH;
-        var b = [_]u32{0} ** MAX_WIDTH;
+        var a = [_]u64{0} ** MAX_WIDTH;
+        var b = [_]u64{0} ** MAX_WIDTH;
 
         // "Segment" is a sequence of ###s in the input, specified by a number in the
         // second half of the input.
         // The minimum width of a valid pattern is the sum of all segments + one space
         // between each segment.
-        const min_segments_width = sum(self.segments_buf[0..self.segments_len]) + self.segments_len - 1;
+        const min_segments_width = sum(u32, self.segments_buf[0..self.segments_len]) + self.segments_len - 1;
         // If all N-1 segments are squished at one end of the pattern, the Nth one
         // can be in one of the P position slots:
         const segment_positions = self.width - min_segments_width + 1;
@@ -150,6 +176,9 @@ const Record = struct {
         // valid position slot.
         const first_segmask = Mask.init(self.segments_buf[0], self.width);
         for (0..segment_positions) |i| {
+            if (!self.testEmptyBetween(0, i)) {
+                break;
+            }
             prev[i] = if (self.testPos(i, first_segmask)) 1 else 0;
         }
 
@@ -159,17 +188,27 @@ const Record = struct {
         for (1..self.segments_len) |s| {
             const segment = self.segments_buf[s];
             const segmask = Mask.init(segment, self.width);
-            // reset valid options
-            var valid_options: u32 = 0;
             for (0..segment_positions) |i| {
-                // Assuming that segment S is at position i, the segment S-1 could be
-                // placed on any position slot up to i.
-                // So the number of options for S is the sum of all options for S-1
-                // on all the previous slots.
-                // valid_options is an accumulator for this number.
-                valid_options += prev[i];
+                // clear current slot
+                cur[i] = 0;
                 const valid = self.testPos(ofs + i, segmask);
-                cur[i] = if (valid) valid_options else 0;
+                if (!valid) {
+                    // zero options if the segment can't be placed here
+                    continue;
+                }
+                // Assuming that segment S is at position i, the segment S-1 could be
+                // placed on any position slot up to i...
+                // ...unless there is a required piece in the empty space.
+                // So the number of options for S is the sum of all options for S-1
+                // on all the previous slots up to the first one that leaves out
+                // the required piece.
+                for (0..i + 1) |j| {
+                    const empty_start = ofs + i - j;
+                    if (!self.testEmptyBetween(empty_start, j)) {
+                        break;
+                    }
+                    cur[i] += prev[i - j];
+                }
             }
 
             // move on to the next segment:
@@ -181,30 +220,82 @@ const Record = struct {
             cur = tmp;
         }
 
+        // correction for the impossible positions of the last segment at the end of the pattern
+        // ofs now points to the position of the "next segment" after the last one
+        for (0..segment_positions) |i| {
+            if (!self.testEmptyBetween(ofs + i, segment_positions - i)) {
+                // if the space is not (allowed to be) fully empty until the end of the
+                // pattern, the last segment can't be placed here
+                prev[i] = 0;
+            }
+        }
+
         // all options is the sum of options of the last segment on all position slots
-        return sum(prev[0..segment_positions]);
+        return sum(u64, prev[0..segment_positions]);
     }
 
-    fn numMask(num: u32) u64 {
-        var mask: u64 = 0;
+    pub fn enumerateOptions(self: Record) u32 {
+        // "Segment" is a sequence of ###s in the input, specified by a number in the
+        // second half of the input.
+        // The minimum width of a valid pattern is the sum of all segments + one space
+        // between each segment.
+        const min_segments_width = sum(u32, self.segments_buf[0..self.segments_len]) + self.segments_len - 1;
+        // If all N-1 segments are squished at one end of the pattern, the Nth one
+        // can be in one of the P position slots:
+        const segment_positions = self.width - min_segments_width + 1;
+        // Every segment has P slots, but they are not the same real positions:
+        // slot 0 for segment 0 is at offset 0 from start, but slot 0 for segment 1 is
+        // at offset (slot 0 width + 1) to account for segment 0 at start, etc.
+
+        // now the debug part
+        return self.genOptions(0, 0, 0, 0, segment_positions);
+    }
+
+    fn genOptions(self: Record, patmask: u128, n: usize, ofs: usize, startpos: usize, segment_positions: usize) u32 {
+        if (n >= self.segments_len) {
+            if (self.used_mask & patmask != self.used_mask) {
+                return 0;
+            }
+            if (self.empty_mask & ~patmask != self.empty_mask) {
+                return 0;
+            }
+            return 1;
+        }
+        const seg = self.segments_buf[n];
+        const segmask = Mask.init(seg, self.width);
+
+        var total: u32 = 0;
+
+        for (startpos..segment_positions) |pos| {
+            if (!self.testPos(ofs + pos, segmask)) {
+                continue;
+            }
+            const newmask = patmask | (segmask.num_mask << @intCast(ofs + pos));
+            total += self.genOptions(newmask, n + 1, ofs + seg + 1, pos, segment_positions);
+        }
+        return total;
+    }
+
+    fn numMask(num: u32) u128 {
+        var mask: u128 = 0;
         for (0..num) |i| {
-            mask |= @as(u64, 1) << @intCast(i);
+            mask |= @as(u128, 1) << @intCast(i);
         }
         return mask;
     }
 
-    fn printBin(self: Record, mask: u64) void {
-        for (0..self.width + 3) |i| {
+    fn printBin(self: Record, mask: u128, prev: u64) void {
+        for (0..self.width) |i| {
             const ch: u8 = if (mask & (ONE << @intCast(i)) > 0) '#' else '.';
             std.debug.print("{c}", .{ch});
         }
-        std.debug.print("\n", .{});
+        std.debug.print(" {}\n", .{prev});
     }
 
     fn printPattern(self: Record) void {
         for (0..self.width) |i| {
-            const bit: u64 = ONE << @intCast(i);
-            const ch: u8 = if (self.can_be_used_mask & bit > 0 and self.can_be_empty_mask & bit > 0) '?' else if (self.can_be_used_mask & bit > 0) '#' else if (self.can_be_empty_mask & bit > 0) '.' else ' ';
+            const bit: u128 = ONE << @intCast(i);
+            const ch: u8 = if (self.any_mask & bit > 0) '?' else if (self.used_mask & bit > 0) '#' else if (self.empty_mask & bit > 0) '.' else ' ';
             std.debug.print("{c}", .{ch});
         }
         std.debug.print("\n", .{});
@@ -229,12 +320,21 @@ const Record = struct {
         return true;
     }
 
+    fn testEmptyBetween(self: Record, start: usize, shift: usize) bool {
+        const used_mask = self.used_mask >> @intCast(start);
+        const clear_mask = ~(~@as(u128, 0) << @intCast(shift));
+        const res = used_mask & clear_mask == 0;
+        return res;
+    }
+
     fn testPos(self: Record, pos: usize, mask: Mask) bool {
         // std.debug.print("TEST:\n", .{});
         // self.printPattern();
         // std.debug.print("\n", .{});
         // std.debug.print("\n", .{});
-        const ret = mask.testNum(self.can_be_used_mask, pos) and mask.testSpaces(self.can_be_empty_mask, pos);
+        const used_mask = self.used_mask | self.any_mask;
+        const empty_mask = self.empty_mask | self.any_mask;
+        const ret = mask.testNum(used_mask, pos) and mask.testSpaces(empty_mask, pos);
         // if (ret) {
         //     const mmask = mask.num_mask << @intCast(pos);
         //     self.printBin(mmask);
@@ -248,7 +348,7 @@ const Record = struct {
 
 test "test" {
     const in = "?###???????? 3,2,1";
-    try expectEqual(Record.parse(in).dynCountOptions(), 10);
+    try expectEqual(Record.parse(in).enumerateOptions(), 10);
 }
 
 test "part1 example" {
@@ -265,6 +365,48 @@ test "part1 example" {
     try expect(std.mem.eql(u8, result, "21"));
 }
 
+test "manual expansion" {
+    const in = "???.###????.###????.###????.###????.### 1,1,3,1,1,3,1,1,3,1,1,3,1,1,3";
+    try expectEqual(Record.parse(in).enumerateOptions(), 1);
+}
+
+test "dyn counterexample" {
+    const in = "??#???? 1,1";
+    const rec = Record.parse(in);
+    const good_result = rec.enumerateOptions();
+    const test_result = rec.dynCountOptions();
+    try expectEqual(good_result, @intCast(test_result));
+}
+
+test "dyn failing case" {
+    const in = ".#?????####.?.#? 1,1,5,1";
+    const rec = Record.parse(in);
+    const good_result = rec.enumerateOptions();
+    const test_result = rec.dynCountOptions();
+    try expectEqual(good_result, @intCast(test_result));
+}
+
+test "part2" {
+    const in = "???.### 1,1,3";
+    const expanded = "???.###????.###????.###????.###????.### 1,1,3,1,1,3,1,1,3,1,1,3,1,1,3";
+    var rec = Record.parse(in);
+    rec.expand(5);
+    const expected = Record.parse(expanded);
+    try expectEqual(rec.used_mask, expected.used_mask);
+    try expectEqual(rec.empty_mask, expected.empty_mask);
+    try expectEqual(rec.any_mask, expected.any_mask);
+    try expectEqual(rec.width, expected.width);
+    try expectEqual(rec.segments_len, expected.segments_len);
+    try expectEqual(rec.enumerateOptions(), 1);
+}
+
+test "part2more" {
+    const in = ".??..??...?##. 1,1,3";
+    var rec = Record.parse(in);
+    rec.expand(5);
+    try expectEqual(rec.enumerateOptions(), 16384);
+}
+
 pub fn part1(data: []const u8, alloc: std.mem.Allocator, result_buf: []u8) anyerror![]const u8 {
     _ = alloc;
 
@@ -279,7 +421,16 @@ pub fn part1(data: []const u8, alloc: std.mem.Allocator, result_buf: []u8) anyer
 }
 
 pub fn part2(data: []const u8, alloc: std.mem.Allocator, result_buf: []u8) anyerror![]const u8 {
-    _ = data;
     _ = alloc;
-    return result_buf;
+
+    var total: usize = 0;
+    var lines_iter = std.mem.tokenizeScalar(u8, data, '\n');
+    while (lines_iter.next()) |line| {
+        var rec = Record.parse(line);
+        rec.expand(5);
+        const result = rec.dynCountOptions();
+        total += result;
+    }
+
+    return std.fmt.bufPrint(result_buf, "{}", .{total});
 }
