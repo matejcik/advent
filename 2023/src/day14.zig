fbbe0a5972c560e71d8828cfe0d72cbaaed0b7fd
matejcik <ja@matejcik.cz>
2024-01-03 11:37:28 +0100

    2023: day 14 tweaked & fixed

--- 2023/src/day14.zig
+++ 2023/src/day14.zig
@@ -10,7 +10,7 @@ const SIMD_WORDS = SIMD_WIDTH_BITS / (8 * @sizeOf(u8));
 
 const BoolVec = @Vector(SIMD_WORDS, bool);
 const U8Vec = @Vector(SIMD_WORDS, u8);
-const U32Vec = @Vector(SIMD_WORDS, u32);
+const U16Vec = @Vector(SIMD_WORDS, u16);
 
 const ONES: U8Vec = @splat(1);
 const ZEROES: U8Vec = @splat(0);
@@ -45,30 +45,52 @@ pub fn part1novec(data: []const u8) u32 {
             }
         }
     }
+    // std.debug.print("\nnovec column totals: ", .{});
+    // for (0..grid.width) |x| {
+    //     std.debug.print("{} ", .{totals[x]});
+    // }
+    // std.debug.print("\n", .{});
 
     return sum(u32, &totals);
 }
 
 pub fn part1vec(data: []const u8, alloc: std.mem.Allocator) u32 {
+    const gridpat = Grid.new(.{ .immutable = data }, '\n');
+
     // preallocate a longer buffer, so that when we try to read a line whose width is
     // a multiple of SIMD_WORDS, all the data is still in the buffer
     // (the trailing part is going to be noise from the next line, but we don't care)
-    var data_extended = alloc.alloc(u8, data.len + MAX_WIDTH) catch unreachable;
+    var data_extended = alloc.alloc(u8, data.len + SIMD_WORDS) catch unreachable;
     defer alloc.free(data_extended);
     @memcpy(data_extended[0..data.len], data);
 
-    const grid = Grid.new(.{ .immutable = data_extended }, '\n');
+    var grid = Grid.new(.{ .immutable = data_extended }, '\n');
+    grid.height = gridpat.height;
 
-    // set up the vectors:
-    // breaks are the #stops for each column
-    var breaks = [_]U8Vec{@splat(@intCast(grid.height))} ** (MAX_WIDTH / SIMD_WORDS);
-    // stones are the number of stones already piled on the previous break
-    var stones = [_]U8Vec{@splat(0)} ** (MAX_WIDTH / SIMD_WORDS);
-    // totals are the sum of the stone values
-    var totals = [_]U32Vec{@splat(0)} ** (MAX_WIDTH / SIMD_WORDS);
+    const Item = struct {
+        breaks: U8Vec,
+        stones: U8Vec,
+        totals: U16Vec,
+    };
+
+    const elems = (grid.width + SIMD_WORDS - 1) / SIMD_WORDS;
+
+    var array = [_]Item{.{
+        .breaks = @splat(@intCast(grid.height)),
+        .stones = @splat(0),
+        .totals = @splat(0),
+    }} ** MAX_WIDTH;
+
+    // // set up the vectors:
+    // // breaks are the #stops for each column
+    // var breaks = [_]U8Vec{@splat(@intCast(grid.height))} ** (MAX_WIDTH / SIMD_WORDS);
+    // // stones are the number of stones already piled on the previous break
+    // var stones = [_]U8Vec{@splat(0)} ** (MAX_WIDTH / SIMD_WORDS);
+    // // totals are the sum of the stone values
+    // var totals = [_]U16Vec{@splat(0)} ** (MAX_WIDTH / SIMD_WORDS);
 
     // number of vectors that fit the whole line
-    const elems = (grid.width + SIMD_WORDS - 1) / SIMD_WORDS;
+    // const elems = (grid.width + SIMD_WORDS - 1) / SIMD_WORDS;
 
     for (0..grid.height) |y| {
         // grab index of the first element of the line
@@ -78,57 +100,75 @@ pub fn part1vec(data: []const u8, alloc: std.mem.Allocator) u32 {
         // vec of next values
         const simd_next_value: U8Vec = @splat(@intCast(grid.height - y - 1));
 
-        for (0..elems) |elem| {
+        for (array[0..elems], 0..) |*item, elem| {
             const ofs = elem * SIMD_WORDS;
             const segment = line[ofs..][0..SIMD_WORDS];
+
             const simd_segment: U8Vec = segment.*;
             const simd_blocks = simd_segment == @as(U8Vec, @splat('#'));
             const simd_stones = simd_segment == @as(U8Vec, @splat('O'));
-            // if char == '0' {
+            // if char == 'O' {
             //     totals[x] += breaks[x] - stones[x];
-            totals[elem] += @select(u8, simd_stones, breaks[elem] - stones[elem], ZEROES);
+            item.totals += @select(u8, simd_stones, item.breaks - item.stones, ZEROES);
             //     stones[x] += 1;
-            stones[elem] += @select(u8, simd_stones, ONES, ZEROES);
+            item.stones += @select(u8, simd_stones, ONES, ZEROES);
             // } else if char == '#' {
             //     stones[x] = 0;
-            stones[elem] = @select(u8, simd_blocks, ZEROES, stones[elem]);
+            item.stones = @select(u8, simd_blocks, ZEROES, item.stones);
             //     breaks[x] = @intCast(value - 1);
-            breaks[elem] = @select(u8, simd_blocks, simd_next_value, breaks[elem]);
+            item.breaks = @select(u8, simd_blocks, simd_next_value, item.breaks);
         }
-
-        // for (line, 0..) |ch, x| {
-        //     switch (ch) {
-        //         '#' => {
-        //             breaks[x] = @intCast(value - 1);
-        //             stones[x] = 0;
-        //         },
-        //         'O' => {
-        //             totals[x] += breaks[x] - stones[x];
-        //             stones[x] += 1;
-        //         },
-        //         else => {},
-        //     }
-        // }
     }
     // collect totals
     var total: u32 = 0;
     for (0..grid.width / SIMD_WORDS) |i| {
-        total += @reduce(.Add, totals[i]);
+        total += @reduce(.Add, array[i].totals);
     }
     if (grid.width % SIMD_WORDS != 0) {
-        const lastvec = totals[grid.width / SIMD_WORDS];
+        const lastvec = array[grid.width / SIMD_WORDS].totals;
         for (0..grid.width % SIMD_WORDS) |i| {
             total += lastvec[i];
         }
     }
 
+    // std.debug.print("\nsimd column totals: ", .{});
+    // for (0..grid.width) |x| {
+    //     const simd = totals[x / SIMD_WORDS];
+    //     const totall = simd[x % SIMD_WORDS];
+    //     std.debug.print("{} ", .{totall});
+    // }
+    // std.debug.print("\n", .{});
+
     return total;
+}
+
+const PART1_EXAMPLE =
+    \\O....#....
+    \\O.OO#....#
+    \\.....##...
+    \\OO.#O....O
+    \\.O.....O#.
+    \\O.#..O.#.#
+    \\..O..#O..O
+    \\.......O..
+    \\#....###..
+    \\#OO..#....
+    \\
+;
+
+test "part1 example novec" {
+    const total = part1novec(PART1_EXAMPLE);
+    try std.testing.expectEqual(total, 136);
+}
+
+test "part1 example vec" {
+    const total = part1vec(PART1_EXAMPLE, std.testing.allocator);
+    try std.testing.expectEqual(total, 136);
 }
 
 pub fn part1(data: []const u8, alloc: std.mem.Allocator, result_buf: []u8) anyerror![]const u8 {
     // _ = alloc;
     // const total = part1novec(data);
-
     const total = part1vec(data, alloc);
     return std.fmt.bufPrint(result_buf, "{}", .{total});
 }
