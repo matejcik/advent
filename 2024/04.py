37b973e693552cf994d895ddca598eb8cdfa92dd
matejcik <ja@matejcik.cz>
2024-12-05 21:36:40 +0100

    2024 wip: improved perf but broken code for day 4

--- 2024/04.py
+++ 2024/04.py
@@ -1,8 +1,11 @@
 import numpy as np
 
-NEEDLE = np.array(list(b"XMAS"), dtype=np.uint8)
+import adventlib
+
+NEEDLE = b"XMAS"
 N = len(NEEDLE)
 NEEDLE_BACKWARDS = NEEDLE[::-1]
+
 
 def load_input() -> np.ndarray:
     lines = []
@@ -13,41 +16,50 @@ def load_input() -> np.ndarray:
 
 
 def testneedle(haystack: np.array) -> bool:
-    return np.array_equal(haystack, NEEDLE) or np.array_equal(haystack, NEEDLE_BACKWARDS)
+    b = haystack.tobytes()
+    return b == NEEDLE or b == NEEDLE_BACKWARDS
 
 
-def part1_search_xmas(data: np.ndarray) -> int:
+def diagonal_shift(data: np.ndarray) -> np.ndarray:
+    newdata = data.copy()
     x, y = data.shape
-    total = 0
-    for i in range(x):
+    for i in range(y):
+        newdata[i, :] = np.concatenate((data[i, i:], data[i, :i]))
+    return newdata
+
+
+@adventlib.timeit(2)
+def part1_search_xmas(data: np.ndarray) -> int:
+    transposed = data.transpose()
+    diag_a = diagonal_shift(data)
+    diag_b = diagonal_shift(np.fliplr(data))
+
+    def find_in(data: np.ndarray) -> int:
+        total = 0
+        x, y = data.shape
         for j in range(y):
-            if testneedle(data[i, j:j + N]):
-                total += 1
-            if testneedle(data[i:i + N, j].transpose()):
-                total += 1
-            diag = data[i:i + N, j:j + N]
-            if testneedle(diag.diagonal()):
-                total += 1
-            if testneedle(np.fliplr(diag).diagonal()):
-                total += 1
+            for i in range(x):
+                if testneedle(data[i : i + N, j]):
+                    total += 1
+        return total
 
-    return total
+    return find_in(data) + find_in(transposed) + find_in(diag_a) + find_in(diag_b)
 
+
+@adventlib.timeit()
 def part2_search_x_mas(data: np.ndarray) -> int:
     x, y = data.shape
     A = ord("A")
     M = ord("M")
     S = ord("S")
     total = 0
-    for i in range(x - 2):
-        for j in range(y - 2):
-            testregion = data[i:i + 3, j:j + 3]
-            assert testregion.shape == (3, 3)
-            if testregion[1,1] != A:
+    for i in range(1, x - 1):
+        for j in range(1, y - 1):
+            if data[i, j] != A:
                 continue
-            diag_a = testregion.diagonal()
-            diag_b = np.fliplr(testregion).diagonal()
-            if M in diag_a and S in diag_a and M in diag_b and S in diag_b:
+            diag_a = data[i - 1, j - 1], data[i + 1, j + 1]
+            diag_b = data[i - 1, j + 1], data[i + 1, j - 1]
+            if M in diag_a and M in diag_b and S in diag_a and S in diag_b:
                 total += 1
     return total
 
